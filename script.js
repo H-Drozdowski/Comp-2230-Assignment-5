26141bdfb3955ba5639f937a0c66eaee202ec799
H-Drozdowski <hdrozdowski@rrc.ca>
2026-03-19 11:16:43 -0500

    feat: Created a function that fetches the json, and one that lists the json info in the console for testing.

--- script.js
+++ script.js
@@ -2,4 +2,29 @@
 Hudson Drozdowski
 Assignment 5
 Java Script
+
+https://data.winnipeg.ca/resource/hfwk-jp4h.json
 */
+
+async function getTrees(){
+    try{
+        const response = await fetch("https://data.winnipeg.ca/resource/hfwk-jp4h.json")
+
+        if (!response.ok){
+            throw new Error(`HTTP Error! status: ${response.status}`)
+        }
+
+        return response.json()
+    }
+    catch(error){
+        // Logs the error message
+        console.error("Failed to fetch countries", error.message)
+    }
+}
+
+async function listTrees(){
+    const trees = await getTrees()
+    console.log(trees)
+}
+
+listTrees()
