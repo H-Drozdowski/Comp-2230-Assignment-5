7391b1e8bce90d85351ab44ed361ce1cc0776c2c
H-Drozdowski <hdrozdowski@rrc.ca>
2026-03-21 00:14:57 -0500

    feat: Added a function that creates a info box for each tree.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 /*
-Hudson Drozdowski
-Assignment 5
-Java Script
-
-https://data.winnipeg.ca/resource/hfwk-jp4h.json
+    Hudson Drozdowski
+    Assignment 5
+    Java Script
+    https://data.winnipeg.ca/resource/hfwk-jp4h.json
 */
 
 const searchButton = document.getElementById("searchButton")
@@ -27,7 +26,7 @@ async function getTrees(){
 
 async function searchPark(park_name){
     try{
-        const response = await fetch(`https://data.winnipeg.ca/resource/hfwk-jp4h.json?$where=park='${park_name}'&$limit=25`)
+        const response = await fetch(`https://data.winnipeg.ca/resource/hfwk-jp4h.json?$where=park='${park_name}'&$limit=100`)
 
         if (!response.ok){
             throw new Error(`HTTP Error! status: ${response.status}`)
@@ -41,9 +40,35 @@ async function searchPark(park_name){
     }
 }
 
+const createTreeInfoBox = ( (commonName, treeName, treeId, treeWard) => {
+    const treeContainer = document.createElement("div")
+    const treeType = document.createElement("p")
+    const commonTreeName = document.createElement("p")
+    const treeIdPar = document.createElement("p")
+    const treeWardPar = document.createElement("p")
+
+    treeType.textContent = `Botanical Name:  ${treeName}`
+    commonTreeName.textContent = `Common Name:  ${commonName}`
+    treeIdPar.textContent = `Tree ID:  ${treeId}`
+    treeWardPar.textContent = `Tree Electoral Ward:  ${treeWard}`
+
+    treeContainer.append(treeIdPar)
+    treeContainer.append(commonTreeName)
+    treeContainer.append(treeType)
+    treeContainer.append(treeWardPar)
+    document.body.append(treeContainer)
+
+    treeContainer.className = "treeContainer"
+}) 
+
+
 async function listTrees(){
     const trees = await searchPark(searchBar.value)
     console.log(trees)
+
+    for (const tree of trees){
+        createTreeInfoBox(tree["common_name"], tree["botanical_name"], tree["tree_id"], tree["electoral_ward"])
+    }
 }
 
 searchButton.addEventListener("click", () => {
